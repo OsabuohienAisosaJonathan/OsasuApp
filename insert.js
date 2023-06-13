3cf13bd81036799bf8f9419daaf84ab5d3da2141
unknown <OsabuohienAisosaJonathan>
2023-06-13 11:47:33 +0100

    small correction

--- insert.js
+++ insert.js
@@ -13,6 +13,6 @@ con.connect(function(err) {
   var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("1 record inserted");
+    console.log("one record inserted");
   });
 });
