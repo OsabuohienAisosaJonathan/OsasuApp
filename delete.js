ca1c8b422eb880e4000726dc893ae7a480cfee62
unknown <OsabuohienAisosaJonathan>
2023-06-13 11:38:06 +0100

    Addition correction

---
+++ delete.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+//connection to mysql database 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "anthas"
+});
+
+//operation for deleting a specific record
+con.connect(function(err) {
+    if (err) throw err;
+    var sql ="DELETE FROM customers WHERE address = 'Mountain 21'";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Number of records deleted: " + result.affectedRows);
+    });
+});
